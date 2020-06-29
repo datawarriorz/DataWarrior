e6f79ae2937c9a97fac56bb41127dc9f0cb034f5
datawarriorz <66295952+datawarriorz@users.noreply.github.com>
2020-06-29 21:17:52 +0530

    Update Internship.php

--- app/Http/Controllers/Internship.php
+++ app/Http/Controllers/Internship.php
@@ -38,6 +38,20 @@ class Internship extends Controller
         $internship->preferreddomain3=$request->preferreddomain3;
         $internship->stipend=$request->stipend;
         $internship->location=$request->location;
+        $skills=new UserSkills();
+        $skills->user_id=Auth::user()->user_id;
+        $skills->skill1=$request->skill1;
+        $skills->skill2=$request->skill2;
+        $skills->skill3=$request->skill3;
+        $jobexp =new Jobexperience();
+        $jobexp->user_id=Auth::user()->user_id;
+        $jobexp->profile=$request->profile;
+        $jobexp->organisation=$request->organisation;
+        $jobexp->location=$request->location;
+        $jobexp->startdate=$request->startdate;
+        $jobexp->enddate=$request->enddate;
+        $jobexp->description=$request->description;
+        $jobexp->currentjob=$request->currentjob;
         
 
         $validator=Validator::make($request->all(), [
@@ -46,7 +60,16 @@ class Internship extends Controller
             'preferreddomain3' => 'required|min:3',
             'stipend' => 'required|numeric',
             'location' => 'required',
-            
+            'skill1' => 'required|min:3',
+            'skill2' => 'required|min:3',
+            'skill3' => 'required|min:3',
+            'profile' => 'required|min:3',
+            'organisation' => 'required|min:3',
+            'location' => 'required|min:3',
+            'description' => 'required|min:3',
+            'enddate' =>'required',
+            'startdate' =>'required',
+
             
             
         ],[]);
@@ -57,62 +80,10 @@ class Internship extends Controller
         
 
         $internship->save();
+        $skills->save();
+        $jobexp->save();
+
       
     }
-    public function internshipSkills(Request $request){
-
-        $validator=Validator::make($request->all(), [
-            'skill1' => 'required|min:3',
-            'skill2' => 'required|min:3',
-            'skill3' => 'required|min:3',
-            
-        ],[]);
-        if ($validator->fails()) // on validator found any error 
-      {
-        return redirect('/internship')->withErrors($validator)->withInput();
-      }
-      $skills=new UserSkills();
-      $skills->user_id=Auth::user()->user_id;
-      $skills->skill1=$request->skill1;
-      $skills->skill2=$request->skill2;
-      $skills->skill3=$request->skill3;
-    
-      $skills->save();
-    }
-
-    public function internshipExp(Request $request){
-
-         
-    $jobexp =new Jobexperience();
-    $jobexp->user_id=Auth::user()->user_id;
-    $jobexp->profile=$request->profile;
-    $jobexp->organisation=$request->organisation;
-    $jobexp->location=$request->location;
-    $jobexp->startdate=$request->startdate;
-    $jobexp->enddate=$request->enddate;
-    $jobexp->description=$request->description;
-    $jobexp->currentjob=$request->currentjob;
-    
-    //dd($qualification);
-
-    $validator=Validator::make($request->all(), [
-        'profile' => 'required|min:3',
-        'organisation' => 'required|min:3',
-        'location' => 'required|min:3',
-        'description' => 'required|min:3',
-        'enddate' =>'required',
-        'startdate' =>'required',
-        
-    ],[]);
-    if ($validator->fails()) // on validator found any error 
-  {
-    return redirect('/internship')->withErrors($validator)->withInput();
-  }
-    
-
-    $jobexp->save();
-
-    
-    
-    }
+   
 }

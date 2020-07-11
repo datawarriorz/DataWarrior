f66a8b8dc599f40ba7178dd2b6c71fe28f2ba4c8
datawarriorz <66295952+datawarriorz@users.noreply.github.com>
2020-07-11 09:21:18 +0530

    certification arch

--- app/Http/Controllers/CertificationController.php
+++ app/Http/Controllers/CertificationController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Certification;
+use App\CertificationApplied;
+use App\CertificationRequested;
+
 
 class CertificationController extends Controller
 {
@@ -10,5 +14,21 @@ class CertificationController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->except('logout');
+    }
+
+    public function showallcertification(){
+
+
+
+    }
+    public function requestcertification(Request $request){
+        
+
+
+    }
+    public function certificationdetails(Request $request){
+        
+
+
     }
 }

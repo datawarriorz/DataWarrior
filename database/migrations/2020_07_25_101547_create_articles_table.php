90cc7fa1f11710478cf5b39c73843f7e73599182
datawarriorz <66295952+datawarriorz@users.noreply.github.com>
2020-07-25 22:42:23 +0530

    article

---
+++ database/migrations/2020_07_25_101547_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id('article_id');
+            $table->string('title');
+            $table->unsignedBigInteger('expert_id');
+            $table->foreign('expert_id')->references('id')->on('experts');
+            $table->string('author')->nullable();
+            $table->string('description');
+            $table->string('content');
+            $table->binary('user_image');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}

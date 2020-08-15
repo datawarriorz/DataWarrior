6ff413bc58dc3e693933ea8c8ade132d4dda2e51
datawarriorz <66295952+datawarriorz@users.noreply.github.com>
2020-08-15 22:38:35 +0530

    newsletter on publish and user-side(expert and articles) : backend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ContactUs;
+use App\Expert;
 
 class HomeController extends Controller
 {
@@ -26,7 +27,22 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('home');
+        $expertsobj=Expert::all();
+        return view('home', ['expertsobj'=>$expertsobj]);
+    }
+
+    public function userviewexpert(Request $request)
+    {
+        $expertobj=Expert::where('ex_id', $request->ex_id);
+        $articleslive= Article::where('creator_id', Auth::user()->ex_id)->where('creator_flag', 'expert')->where('status', '=', 'published')->get();
+        return view('', ['expertobj' => $expertobj,'articleslive'=>$articleslive]);
+    }
+    
+    public function userviewarticle(Request $request)
+    {
+        $article_obj= Article::find($request->article_id);
+
+        return view('', ['article' => $article_obj]);
     }
 
     public function test()

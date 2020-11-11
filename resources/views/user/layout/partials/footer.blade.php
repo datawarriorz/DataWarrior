cb04d5ca403fffab55d96e0ccff84feffd13d6b6
Robin Varughese <65695569+RRVarghese@users.noreply.github.com>
2020-11-12 00:21:49 +0530

    footer update

--- resources/views/user/layout/partials/footer.blade.php
+++ resources/views/user/layout/partials/footer.blade.php
@@ -13,22 +13,16 @@
                                     <th class="footer-link-header">SERVICES</th>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Marketing</a></td>
+                                    <td><a class="footer-link" href="#">Internships</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Sales Enablement</a></td>
+                                    <td><a class="footer-link" href="#">Jobs</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Services</a></td>
+                                    <td><a class="footer-link" href="#">Certifications</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Design</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Pricing</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Start Growing</a></td>
+                                    <td><a class="footer-link" href="#">Projects</a></td>
                                 </tr>
                             </table>
                         </div>
@@ -39,25 +33,16 @@
                         <div class="footer-container">
                             <table class="table footer1-table">
                                 <tr>
-                                    <th class="footer-link-header">CHALLENGE</th>
+                                    <th class="footer-link-header">KNOWLEDGE FORUM</th>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Lead Generation</a></td>
+                                    <td><a class="footer-link" href="#">Blogs</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">SFO</a></td>
+                                    <td><a class="footer-link" href="#">Articles</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Sales</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Content Marketing</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Website Optimization</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Email Marketing</a></td>
+                                    <td><a class="footer-link" href="#">Expert Speak</a></td>
                                 </tr>
                             </table>
                         </div>
@@ -68,48 +53,36 @@
                         <div class="footer-container">
                             <table class="table footer1-table">
                                 <tr>
-                                    <th class="footer-link-header">SOLUTIONS FOR</th>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Executive Leaders</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Marketing Leaders</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Sales Leaders</a></td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6 col-lg-3">
-                    <div class="col-sm-12 col-md-12 col-lg-12">
-                        <div class="footer-container">
-                            <table class="table footer1-table">
-                                <tr>
-                                    <th class="footer-link-header">RESOURCES</th>
+                                    <th class="footer-link-header">SUPPORT</th>
                                 </tr>
                                 <tr>
                                     <td><a class="footer-link" href="#">About Us</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Careers</a></td>
+                                    <td><a class="footer-link" href="#">Contact Us</a></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <div class="col-sm-12 col-md-12 col-lg-12">
+                        <div class="footer-container">
+                            <table class="table footer1-table">
+                                <tr>
+                                    <th class="footer-link-header">OUR POLICIES</th>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Get a HubSpot Demo</a></td>
+                                    <td><a class="footer-link" href="#">Privacy Policy</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">What is HubSpot?</a></td>
+                                    <td><a class="footer-link" href="#">Disclaimer</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Blog</a></td>
+                                    <td><a class="footer-link" href="#">CopyRights</a></td>
                                 </tr>
                                 <tr>
-                                    <td><a class="footer-link" href="#">Resource Library</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a class="footer-link" href="#">Case Studies</a></td>
+                                    <td><a class="footer-link" href="#">Terms & Conditions</a></td>
                                 </tr>
                             </table>
                         </div>
